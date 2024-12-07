ec7a5a9e15e0c61eedec9741d588ce4affe714d9
sandkalam <sandykalamuddin07@gmail.com>
2024-12-07 14:45:08 +0700

    fix bug

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 // chat
 
 // nama
-let nama = localStorage.getItem("nama");
+// let nama = localStorage.getItem("nama");
 
-if (nama === null) {
-  document.getElementById("popup_awal").style.display = "block";
-  document.getElementById("root").style.display = "none";
-} else {
-  // document.getElementById("nama_hasil").innerHTML = nama;
-}
+// if (nama === null) {
+//   document.getElementById("popup_awal").style.display = "block";
+//   document.getElementById("root").style.display = "none";
+// } else {
+//   // document.getElementById("nama_hasil").innerHTML = nama;
+// }
 
 // ketika mengenter di bagian input nama, nama akan disimpan di local storage, dan halaman akan di refresh
-document
-  .getElementById("inputNama")
-  .addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      handleNama();
-    }
-  });
-function handleNama() {
-  let nama = document.getElementById("inputNama");
+// document
+//   .getElementById("inputNama")
+//   .addEventListener("keypress", function (event) {
+//     if (event.key === "Enter") {
+//       handleNama();
+//     }
+//   });
+// function handleNama() {
+//   let nama = document.getElementById("inputNama");
 
-  // menghilangkan karakter jahat
-  let namaValid = nama.value.replace(/[^A-Za-z ]/g, "");
-  localStorage.setItem("nama", namaValid);
+//   // menghilangkan karakter jahat
+//   let namaValid = nama.value.replace(/[^A-Za-z ]/g, "");
+//   localStorage.setItem("nama", namaValid);
 
-  window.location.reload();
-}
+//   window.location.reload();
+// }
 
 // daftar pelatihan-1
 fetch("/api/belajar_html5.json")
