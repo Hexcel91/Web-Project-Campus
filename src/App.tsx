960b360d77cf6c8744f189f7fb8a21f249c00d5a
sandkalam <sandykalamuddin07@gmail.com>
2024-12-08 18:32:25 +0700

    🎉 init: try to using react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veritatis
+        quia officia.
+      </h1>
+      <Button>Helow lord</Button>
+    </>
+  );
+}
+
+export default App;
